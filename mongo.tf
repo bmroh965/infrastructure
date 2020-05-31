b6403675a44a19262d6d476c440f51ad5a922e8c
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-05-31 16:45:25 -0700

    Make route creation DRY

--- mongo.tf
+++ mongo.tf
@@ -31,24 +31,11 @@ resource "aws_vpc_peering_connection_accepter" "peer" {
   auto_accept = true
 }
 
-# Route tables for peering connection
-# TODO figure out how to make this DRY using a loop
-resource "aws_route" "peering-owner-az1" {
-  route_table_id            = module.vpc.private_route_table_ids[0]
-  destination_cidr_block    = "10.8.0.0/21"
+resource "aws_route" "peering_to_owner" {
+  for_each = toset(module.vpc.private_route_table_ids)
+  destination_cidr_block = "10.8.0.0/21"
   vpc_peering_connection_id = mongodbatlas_network_peering.peer.connection_id
-}
-
-resource "aws_route" "peering-owner-az2" {
-  route_table_id            = module.vpc.private_route_table_ids[1]
-  destination_cidr_block    = "10.8.0.0/21"
-  vpc_peering_connection_id = mongodbatlas_network_peering.peer.connection_id
-}
-
-resource "aws_route" "peering-owner-az3" {
-  route_table_id            = module.vpc.private_route_table_ids[2]
-  destination_cidr_block    = "10.8.0.0/21"
-  vpc_peering_connection_id = mongodbatlas_network_peering.peer.connection_id
+  route_table_id = each.value
 }
 
 resource "mongodbatlas_project_ip_whitelist" "whitelist" {
