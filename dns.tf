5dc46c5bd5678f72d5bbf53223f954475f1128e2
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-06-07 12:29:34 -0700

    Create A record in production

--- dns.tf
+++ dns.tf
@@ -30,9 +30,23 @@ resource "aws_acm_certificate_validation" "domain_acm_cert_validation" {
 }
 
 resource "aws_route53_record" "main" {
-  name = "*.${var.domain}"
-  type = "CNAME"
+  count   = var.fp_context == "production" ? 0 : 1
+  name    = "*.${var.domain}"
+  type    = "CNAME"
   zone_id = data.aws_route53_zone.route53_zone_domain.id
   records = [aws_alb.alb_ext.dns_name]
-  ttl = 120
+  ttl     = 120
+}
+
+resource "aws_route53_record" "production" {
+  count   = var.fp_context == "production" ? 1 : 0
+  name    = var.domain
+  type    = "A"
+  zone_id = data.aws_route53_zone.route53_zone_domain.id
+
+  alias {
+    name                   = aws_alb.alb_ext.dns_name
+    zone_id                = aws_alb.alb_ext.zone_id
+    evaluate_target_health = true
+  }
 }
